9dd8d12bfc21d3a746154d9dabf57367abc071e1
EdMBarry <ed.m.barry@gmail.com>
2018-05-30 15:40:02 +1000

    reworked a custom CAPS form

--- source/base/forms.py
+++ source/base/forms.py
@@ -3,7 +3,6 @@ from wtforms import TextField, PasswordField
 
 ## login and registration
 
-
 class LoginForm(FlaskForm):
     username = TextField('Username', id='username_login')
     password = PasswordField('Password', id='pwd_login')
